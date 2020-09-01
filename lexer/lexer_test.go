b2e0bb2dd32ac89f84af9c96c5c73726d70ad188
Alexander Wood <alexljwood24@hotmail.co.uk>
2020-09-01 22:37:02 +0100

    Add a float test

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -20,6 +20,23 @@ func TestIntAssignmentLexing(t *testing.T) {
 		t.Errorf("Incorrect lexing output, got %v but expected %v", tokens, expectedTokens)
 	}
 }
+
+func TestFloatAssignmentLexing(t *testing.T) {
+	code := "let a = 3.5"
+	tokens := lex(code)
+
+	expectedTokens := []Token{
+		CreateToken(LET, "let"),
+		CreateToken(IDENTIFIER, "a"),
+		CreateToken(EQUAL, "="),
+		CreateToken(FLOAT, "3.5"),
+	}
+
+	if !reflect.DeepEqual(tokens, expectedTokens) {
+		t.Errorf("Incorrect lexing output, got %v but expected %v", tokens, expectedTokens)
+	}
+}
+
 func TestStringAssignmentLexing(t *testing.T) {
 	code := `let a = "Hello"`
 	tokens := lex(code)
