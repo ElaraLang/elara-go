a81b95eb1976adf53616545ea1972c954c30014d
Vaishnav Anil <vaishnavanil7th@gmail.com>
2020-09-19 15:13:29 +0530

    Struct field type fix

--- parser/struct.go
+++ parser/struct.go
@@ -46,8 +46,7 @@ func (p *Parser) structField() (field *StructField, err error) {
 	if t1.TokenType == lexer.Identifier {
 		switch t2.TokenType {
 		case lexer.Identifier:
-			typ = ElementaryTypeContract{Identifier: identifier}
-			identifier = t2.Text
+			typ = ElementaryTypeContract{Identifier: t2.Text}
 			if p.match(lexer.Equal) {
 				def, err = p.logicalOr()
 			}
