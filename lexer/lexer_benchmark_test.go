9acf68a61e18410f2c690e6d1be0665f6307b0c2
Alexander Wood <alexwood2403@gmail.com>
2020-10-25 13:58:24 +0000

    The benchmark uses a bigger sample set now

--- lexer/lexer_benchmark_test.go
+++ lexer/lexer_benchmark_test.go
@@ -1,11 +1,11 @@
 package lexer
 
 import (
+	"strings"
 	"testing"
 )
 
-func BenchmarkEverySymbol(b *testing.B) {
-	code := `
+var code = strings.Repeat(`
 		let a = 30
 		let a = 3.5
 		let a = "Hello"
@@ -18,7 +18,10 @@ func BenchmarkEverySymbol(b *testing.B) {
 		+ - * / % && || ^ == != > >= < <= !
 		, : _
 		let	mut	struct if else match while
-		`
+		`, 10_000)
+
+func BenchmarkEverySymbol(b *testing.B) {
+
 	for n := 0; n < b.N; n++ {
 		_ = *Lex(nil, code)
 	}
