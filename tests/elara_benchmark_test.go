bb46b9995d8bdf3eb54940209e4aa9245c1dce83
Alexander Wood <alexwood2403@gmail.com>
2020-11-03 22:32:47 +0000

    Move benchmark to tests package

--- tests/elara_benchmark_test.go
+++ tests/elara_benchmark_test.go
@@ -1,17 +1,16 @@
-package main
+package tests
 
 import (
 	"github.com/ElaraLang/elara/base"
 	"testing"
 )
 
-var code = `let fact = (Int n) => {
-    if n == 1 => return 1
-    return n * fact(n - 1)
-}
-fact(8)`
-
 func BenchmarkElara(b *testing.B) {
+	code := `let fact = (Int n) => {
+	    if n == 1 => return 1
+	    return n * fact(n - 1)
+	}
+	fact(8)`
 	for i := 0; i < b.N; i++ {
 		base.Execute(nil, code, false)
 	}
