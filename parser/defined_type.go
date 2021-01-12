6f7f7391c0414c851d6e6e96faeb3aeee59d6caf
Vaishnav Anil <vaishnavanil7th@gmail.com>
2021-01-12 21:43:04 +0530

    Fixed type node grammar

--- parser/defined_type.go
+++ parser/defined_type.go
@@ -10,6 +10,7 @@ type DefinedType struct {
 func (p *Parser) definedTypes() (types []DefinedType) {
 	types = make([]DefinedType, 0)
 	p.consume(lexer.LBrace, "Expected '{' where defined type starts")
+	p.cleanNewLines()
 	for !p.check(lexer.RBrace) {
 		id := p.consume(lexer.Identifier, "Expected identifier for type in defined type contract")
 		typ := p.primaryContract(true)
@@ -21,7 +22,9 @@ func (p *Parser) definedTypes() (types []DefinedType) {
 		if !p.match(lexer.Comma) {
 			break
 		}
+		p.cleanNewLines()
 	}
+	p.cleanNewLines()
 	p.consume(lexer.RBrace, "Expected '}' where defined type ends")
 	return types
 }
